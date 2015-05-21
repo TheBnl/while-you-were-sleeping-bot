cef864af5a2d67e732751178f1a7c1ad7aea98cf
Bram de Leeuw <contact@bram-de-leeuw.nl>
2015-05-22 00:34:00 +0200

    set up for basic twitter bot

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
 var TwitterBot = require("node-twitterbot").TwitterBot;
 
+/**
+ *  Set up the twitter bot with oAuth details
+ *  The details are stores in local environment variables (.env file)
+ */
 var Bot = new TwitterBot({
-  "consumer_secret": "consumer_secret",
-  "consumer_key": "consumer_key",
-  "access_token": "access_token",
-  "access_token_secret": "access_token_secret"
+  "consumer_secret": process.env.CONSUMER_SECRET,
+  "consumer_key": process.env.CONSUMER_KEY,
+  "access_token": process.env.ACCESS_TOKEN,
+  "access_token_secret": process.env.ACCESS_TOKEN_SECRET
 });
+
+
+/**
+ *  Register the tweet action
+ */
+Bot.addAction("postNewSentence", function(twitter, action, tweet) {
+  var sentence = createNewSentence();
+  console.log('Sending tweet with contents:', sentence);
+  Bot.tweet(sentence);
+});
+
+
+/**
+ *  Sententce Generator
+ *  @author Bram de Leeuw
+ *  @return Generated sentence
+ */
+function createNewSentence() {
+  var sentence = "#WhileYouWereSleeping we were creating a twitter bot";
+  return sentence;
+}
+
+
+/**
+ *  Run the action
+ */
+Bot.now("postNewSentence");
