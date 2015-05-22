9af0841f680ab6df12959fbbfc812c4dafe83587
Bram de Leeuw <contact@bram-de-leeuw.nl>
2015-05-22 14:20:55 +0200

    added sentence generator with quotes from quotbook.com

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var TwitterBot = require("node-twitterbot").TwitterBot;
+var request = require('request');
 
 /**
  *  Set up the twitter bot with oAuth details
@@ -14,32 +15,44 @@ var Bot = new TwitterBot({
 
 /**
  *  Register the tweet action
+ *  Tweet a stripped down sentence from quotbook.com
+ *  @author Bram de Leeuw
  */
 Bot.addAction("postNewSentence", function(twitter, action, tweet) {
-  var sentence = createNewSentence();
-  console.log('Sending tweet with contents:', sentence);
-  //Bot.tweet(sentence);
-});
-
-
-/**
- *  Sententce Generator
- *  @author Bram de Leeuw
- *  @return Generated sentence
- */
-function createNewSentence() {
   var wyws = "#WhileYouWereSleeping";
-  var action = getRandomAction();
-  var partTwo = getRandomEnding(action);
-  var sentence = wyws +" "+ action +" "+ partTwo +".";
-  return sentence;
-}
+  var randomAction = getRandomAction();
+  var baseApi = "http://quotbook.com/widget/~";
+  var query = randomAction.replace(/\s+/g, '-');
+  var type = ".json";
+  var uri = baseApi + query + type;  
+  var sentence;
+
+  request( uri, function (error, response, body) {
+    if (!error && response.statusCode == 200) {    
+      var jsonResponse = JSON.parse(body);
+      var quote = stripQuote(jsonResponse.quote, randomAction);
+      sentence = wyws +" "+ quote +".";
+
+      console.log('LENGTH', sentence.length);
+      console.log('SENTENCE', sentence);
+
+      // TODO at a random roll to tweet or not to tweet
+      // if the action runs every hour in a day it should only tweet three times
+      // chance of 1 in 8
+      if (sentence.length < 140) {
+        //Bot.tweet(sentence);
+      }
+    }
+  });
+});
 
 
 /**
  *  Utility function to retrieve a random item from an Array
  *  @author Bram de Leeuw
- *  @return option
+ *
+ *  @param  Array options
+ *  @return String option
  */
 function getRandom(options) {
   var random = Math.floor(Math.random() * options.length);
@@ -54,7 +67,7 @@ function getRandom(options) {
  */
 function getRandomAction() {
   var actions = [
-    "I",
+    //"I",
     "I was",
     "I put",
     "I dreamt",
@@ -67,6 +80,63 @@ function getRandomAction() {
 
 
 /**
+ *  Strip a string until a action is found
+ *  for example "I dreamt" and split the string
+ *  at it's punctuation.
+ *  @autor Bram de Leeuw
+ *
+ *  @param  String quote
+ *  @param  String action
+ *  @return String first part of stripped quote
+ */
+function stripQuote(quote, action) {
+  var fullQuote = quote.toLowerCase();
+  var strippedQuote = fullQuote.slice( fullQuote.indexOf(action.toLowerCase()) );
+  var splitQuote = strippedQuote.split(/\.|\,/);
+
+  return splitQuote[0];
+}
+
+
+/**
+ *  OBSOLETE
+ *
+ *  Sententce Generator
+ *  @author Bram de Leeuw
+ *
+ *  @return Generated sentence
+ */
+function createNewSentence() {
+  var wyws = "#WhileYouWereSleeping";
+  var action = getRandomAction();
+  // var partTwo = getRandomEnding(action);
+  // var quote = getRandomQuote(action);
+  var baseApi = "http://quotbook.com/widget/~";
+  var query = action.replace(/\s+/g, '-');
+  var type = ".json";
+  var uri = baseApi + query + type;
+  
+  var sentence;
+
+  request( uri, function (error, response, body) {
+    if (!error && response.statusCode == 200) {    
+      var jsonResponse = JSON.parse(body);
+      var quote = stripQuote(jsonResponse.quote, action);
+      sentence = wyws +" "+ quote +".";
+    }
+  });
+
+
+  //sentence = wyws +" "+ action +" "+ partTwo +".";
+
+  console.log("LOGGING"+ sentence);
+  return sentence;
+}
+
+
+/**
+ *  OBSOLETE
+ *
  *  Get a random ending from an array of options
  *  switch options based on the present action
  *  @author Bram de Leeuw
@@ -126,6 +196,33 @@ function getRandomEnding(action) {
   }
 
   return getRandom(options);
+}
+
+
+/**
+ *  OBSOLETE
+ *
+ *  Get a random quote from quotbook.com based on
+ *  the given random action.
+ *  @autor Bram de Leeuw
+ *  @param String action
+ *
+ *  @return String random quote
+ */
+function getRandomQuote(action) {
+  var baseApi = "http://quotbook.com/widget/~";
+  var query = action.replace(/\s+/g, '-');
+  var type = ".json";
+  var uri = baseApi + query + type;
+  var quote;
+  request( uri, function (error, response, body) {
+    if (!error && response.statusCode == 200) {    
+      var jsonResponse = JSON.parse(body);
+      quote = stripQuote(jsonResponse.quote, action);
+      console.log('LOGGING',quote);
+      return quote;
+    }
+  });
 }
 
 
