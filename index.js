3059673d7abdb6661a8a89c4db850148d26df047
Bram de Leeuw <contact@bram-de-leeuw.nl>
2015-05-22 16:32:49 +0200

    added the correct openshift ip

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var port = process.env.OPENSHIFT_NODEJS_PORT || 5000;
+var ipAddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 var baseRouter = express.Router();
 app.use('/', baseRouter);
@@ -10,6 +11,6 @@ baseRouter.route('/')
       res.json({ message: 'Hello, this is the While You Were Sleeping Twitter Bot.' });
   })
 
-app.listen(port, function() {
-  console.log("Node app is running at localhost:" + port);
+app.listen(port, ipAddress, function() {
+  console.log("Node app is running at "+ ipAddress +":"+ port);
 })
