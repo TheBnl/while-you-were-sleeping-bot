0f2e44f6e2dc89f55e98ab7f844472237000829e
Bram de Leeuw <contact@bram-de-leeuw.nl>
2015-05-22 15:57:10 +0200

    fixed the index.js

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 var express = require('express');
 var app = express();
-
-app.set('port', (process.env.OPENSHIFT_NODEJS_PORT || 5000));
+var port = process.env.OPENSHIFT_NODEJS_PORT || 5000;
 
 var baseRouter = express.Router();
+app.use('/', baseRouter);
 
 baseRouter.route('/')
   .get(function(req, res) {
-      res.json({ message: 'While You Were Sleeping Twitter Bot.' });
+      res.json({ message: 'Hello, this is the While You Were Sleeping Twitter Bot.' });
   })
-
 
 app.listen(port, function() {
   console.log("Node app is running at localhost:" + port);
